3a8df966440042ed4576b18b104dc7d83d715539
taylormcgrann <taylormcgrann@stu.indianhills.edu>
2019-12-10 08:30:09 -0600

    my first commit

---
+++ COBTEM00/COBTEM/COBOLTEM00.cbl
@@ -0,0 +1,14 @@
+       identification division.
+       program-id. COBOLTEM00.
+
+       environment division.
+       configuration section.
+
+       data division.
+       working-storage section.
+
+       procedure division.
+
+           goback.
+           
+       end program COBOLTEM00.
